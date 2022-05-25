aacb09c909d12fb09b8566ee971b573de41636b5
GabrielDiaz39 <90275979+GabrielDiaz39@users.noreply.github.com>
2022-05-25 11:26:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/serviceworker.js')
+    navigator.serviceWorker.register('https://gabrieldiaz39.github.io/curriculum/serviceworker.js')
     .then(reg => console.log( reg))
     .catch(error => console.warn(error))
 }
